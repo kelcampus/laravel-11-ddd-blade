fc23a3756ff4b6cbdcc2cdeb84dc592f8c1f7d24
Kelven G. Campos <kelven.campos@corp.saninternet.com>
2024-07-03 09:38:44 -0300

    Work in progress

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: { // To work with Docker Compose
+        host: true,
+        hmr: {
+            host: 'localhost' // If it doesn't work, try replacing it with your local machine's IP
+        },
+        port: 5174
+    },
     plugins: [
         laravel({
             input: [
